fdf2c7a8a639a9ca26a3dc3c13edce3599279ed9
Ahmadwali1 <walia3894@gmail.com>
2023-11-01 14:36:03 +0500

    create form and label in new.ejs

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+
 const exercisesCtrl = require('../controllers/exercises');
 
 // Get /exercises/new
 router.get('/new', exercisesCtrl.new);
+
 module.exports = router;
