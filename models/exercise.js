ac59c76f6a922ed70e372247f46b3abb4e505dad
Ahmadwali1 <walia3894@gmail.com>
2023-11-01 13:26:21 +0500

    create exerciseSchema and export it to model

---
+++ models/exercise.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema;
+
+
+const exerciseSchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  day: {
+    type: String,
+    enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    required: true,
+    default: 'Friday'
+  },
+  bodyPart: {
+    type: String,
+    enum: ['Upper Body', 'Lower Body'],
+    required: true,
+    default: 'Upper Body'
+  },
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Exercise', exerciseSchema);
