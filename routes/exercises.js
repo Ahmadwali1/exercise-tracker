8e070530f631a0058969f67b2397b3f134325acb
Ahmadwali1 <walia3894@gmail.com>
2023-11-01 13:41:00 +0500

    require exerciseCtrl and define new route in exercises.js/routes

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const exercisesCtrl = require('../controllers/exercises');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+// Get /exercises/new
+router.get('/new', exercisesCtrl.new);
 module.exports = router;
