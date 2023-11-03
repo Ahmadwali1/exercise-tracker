2c0d22548b17da37c19cc1fe7b44930007526460
Ahmadwali1 <walia3894@gmail.com>
2023-11-03 22:13:18 +0500

    add ensureLoggedIn to every routes in workouts.js

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const workoutsCtrl = require('../controllers/workouts');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 
 //post /exercises/:id/workouts
-router.post('/exercises/:id/workouts', workoutsCtrl.create);
+router.post('/exercises/:id/workouts',ensureLoggedIn, workoutsCtrl.create);
 
 //delete /exercises/:id/workouts
-router.delete('/exercises/:id/workouts', workoutsCtrl.delete);
+router.delete('/exercises/:id/workouts',ensureLoggedIn, workoutsCtrl.delete);
 
 
 module.exports = router;
