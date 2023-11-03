298d548d8849c754ac7c178868dfc06d9a570e1a
Ahmadwali1 <walia3894@gmail.com>
2023-11-03 21:04:37 +0500

    add passport.use in config/passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+const User = require('../models/user');
 passport.use(new GoogleStrategy(
   // Configuration object
   {
@@ -12,6 +13,30 @@ passport.use(new GoogleStrategy(
   // The verify callback function
   // Let's use async/await!
   async function(accessToken, refreshToken, profile, cb) {
-    // A user has logged in with OAuth...
+    try {
+      // A user has logged in with OAuth...
+      let user = await User.findOne({ googleId: profile.id });
+      // Existing user found, so provide it to passport
+      if (user) return cb(null, user);
+      // We have a new user via OAuth!
+      user = await User.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+      });
+      return cb(null, user);
+    } catch (err) {
+      return cb(err);
+    }
   }
 ));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user._id);
+});
+
+passport.deserializeUser(async function(userId, cb) {
+  // It's nice to be able to use await in-line!
+  cb(null, await User.findById(userId));
+});
